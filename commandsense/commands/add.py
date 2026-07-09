0257d974d8e557cc5e7b34933a88b8c3c424b2cd
jderm503 <jdermerins@gmail.com>
2026-07-09 16:31:43 -0500

    Begin work on transition to sql-based command storage

--- commandsense/commands/add.py
+++ commandsense/commands/add.py
@@ -1,5 +1,6 @@
 from commandsense.db import SQLiteDatabase
 
+
 def register_command(command_parts: list[str], db: SQLiteDatabase) -> None:
     """
     Appends to datastore the command used so that the tool can
@@ -11,8 +12,25 @@ def register_command(command_parts: list[str], db: SQLiteDatabase) -> None:
     Returns:
         _: None
     """
-    command = " ".join(command_parts).strip()
+    command: str = " ".join(command_parts).strip()
     if not command or command.startswith("cmdsense"):
         return
     with open("hold_cmds", "a", encoding="utf-8") as w:
         w.write(f"{command}\n")
+    db.register_command(command)
+
+def register_command_v2(command_parts: list[str], db: SQLiteDatabase) -> None:
+    """
+    Appends to datastore the command used so that the tool can
+    track usage rate of commands and suggest accordingly.
+
+    Args:
+        command(str): The command entered in the terminal by the user
+
+    Returns:
+        _: None
+    """
+    command: str = " ".join(command_parts).strip()
+    if not command or command.startswith("cmdsense"):
+        return
+    db.register_command(command)
