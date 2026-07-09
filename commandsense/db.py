32a636a84c3b64294bd5795babd653649b5bd74c
jderm503 <jdermerins@gmail.com>
2026-07-09 16:32:38 -0500

    Add table creation, command to add to table, and del/close commands for preventing connection hangs

--- commandsense/db.py
+++ commandsense/db.py
@@ -5,6 +5,7 @@ of all logic for the sqlite database which stores
 the commands the user types in normal terminal usage.
 """
 
+from time import time
 from pathlib import Path
 import sqlite3
 
@@ -16,9 +17,65 @@ class SQLiteDatabase:
     """
 
     def __init__(self, db_path: Path) -> None:
-        self.db_path = db_path
-        self.connection = sqlite3.connect(db_path)
-        self.cursor = self.connection.cursor()
+        self.db_path: Path = db_path
+        self.connection: sqlite3.Connection = sqlite3.connect(db_path)
+        self.create_table_if_not_present()
+
+    def __del__(self) -> None:
+        """Auto-call close as a last ditch effort if manual call is removed"""
+        self.close()
+
+    def close(self) -> None:
+        """
+        Closes the active sqlite3 database.
+
+        Returns:
+            _: None
+        """
+        if self.connection is not None:
+            self.connection.close()
+            self.connection = None
+
+    def register_command(self, command: str) -> None:
+        """
+        This function registers a command in the SQLite3 database.
+
+        Args:
+            command (str): The string command
+
+        Returns:
+            _: None
+        """
+        date = int(time())
+        self.connection.execute("""
+            INSERT INTO cmdsense_records (command, uses, last_use_date)
+            VALUES (?, 1, ?)
+            ON CONFLICT(command)
+            DO UPDATE SET
+                uses = uses + 1,
+                last_use_date = excluded.last_use_date
+            """, (command, date))
+        self.connection.commit()
+
+
+    def create_table_if_not_present(self):
+        """
+        This function creates the table in the sqlite3 database if it is not present
+        (such as when program run for the first time).
+
+        Returns:
+            _: None
+        """
+        self.connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS cmdsense_records (
+                command TEXT PRIMARY KEY,
+                uses INTEGER NOT NULL DEFAULT 1,
+                last_use_date INTEGER
+            )
+            """
+        )
+        self.connection.commit()
 
 
     def load_commands_v1(self) -> list[str]:
@@ -28,7 +85,7 @@ class SQLiteDatabase:
         also note that at that point i wont need a load commands function, since i will direct
         hook into sqlite. maybe just a helper function for grabbing requested stuff. will need a
         function for computing frequencies too"""
-        cmds = []
+        cmds: list[str] = []
         with open("hold_cmds", "r", encoding="utf-8") as r:
             for line in r:
                 cmds.append(line.strip())
